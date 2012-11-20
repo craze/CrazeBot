0042bab7179c076ace5075d2a4cd3a1240d07a90
Andrew <Andrew@Indiana>
2012-11-20 15:25:54 -0500

    Added license information.

--- src/main/java/net/bashtech/geobot/PropertiesFile.java
+++ src/main/java/net/bashtech/geobot/PropertiesFile.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2012 Andrew Bashore
+ * This file is part of GeoBot.
+ * 
+ * GeoBot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * GeoBot is distributed in the hope that it will be useful
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with GeoBot.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package net.bashtech.geobot;
 
 import java.io.File;
@@ -19,13 +37,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Used for accessing and creating .[properties] files, reads them as utf-8, saves as utf-8.
- * Internationalization is key importance especially for character codes.
- *
- * @author Nijikokun
- * @version 1.0.4, %G%
- */
 public final class PropertiesFile {
 
     private static final Logger log = Logger.getLogger("GeoBot");
